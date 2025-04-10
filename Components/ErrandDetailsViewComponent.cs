7d66d144640c64a74851b11e4552011ab17c3392
Hossein Esmat Amer <70534576+ha223dp@users.noreply.github.com>
2025-04-10 18:07:22 +0200

    Add files via upload

---
+++ Components/ErrandDetailsViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using EnviromentCrime.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnviromentCrime.Components
+{
+	public class ErrandDetailsViewComponent : ViewComponent
+	{
+		private readonly IerrandsRepository repository;
+
+		public ErrandDetailsViewComponent(IerrandsRepository repository)
+		{
+			this.repository = repository;
+		}
+		// att rätt information kommer up på rätt ställe i var sin respektive Get metod.
+		//och returnerar det som håller fast med det.
+		public Microsoft.AspNetCore.Mvc.IViewComponentResult Invoke(int id)
+		{
+			var errandDetails = repository.GetErrandDetail(id);
+			if (errandDetails.DepartmentId != null)
+			{
+				ViewBag.dept = repository.GetDepartmentDetail(errandDetails.DepartmentId).DepartmentName;
+			}
+			if (errandDetails.EmployeeId != null)
+			{
+				ViewBag.employee = repository.GetEmployeeDetail(errandDetails.EmployeeId).EmployeName;
+			}
+			if (errandDetails.StatusId != null)
+			{
+				ViewBag.status = repository.GetErrandStatus(errandDetails.StatusId).StatusName;
+			}
+
+
+			return View("ItemList", errandDetails);
+		}
+	}
+}
---
+++ Components/RoleCheckerViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using EnviromentCrime.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnviromentCrime.Components
+{
+	public class RoleCheckerViewComponent : ViewComponent
+	{
+		private readonly IerrandsRepository repository;
+
+		public RoleCheckerViewComponent(IerrandsRepository repository)
+		{
+			this.repository = repository;
+		}
+
+		//Kollar genom så att rätt roll är angiven 
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.User = "";
+			if (User.IsInRole("Manager"))
+			{
+				ViewBag.User = "avdelningschef";
+			}
+			if (User.IsInRole("Coordinator"))
+			{
+				ViewBag.User = "samordnare";
+			}
+			else
+			{
+				ViewBag.User = "handläggare";
+			}
+
+			return View("RoleChecker");
+		}
+	}
+}
+
+
