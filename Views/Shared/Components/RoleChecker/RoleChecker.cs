8ccca095c590bc9faa8d490c06e15f11df030ab9
Hossein Esmat Amer <70534576+ha223dp@users.noreply.github.com>
2025-04-10 18:04:55 +0200

    Add files via upload

---
+++ Views/Shared/Components/RoleChecker/RoleChecker.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnviromentCrime.Views.Shared.Components.RoleChecker
+{
+	public class RoleChecker : PageModel
+	{
+		public void onGet()
+		{
+
+		}
+	}
+}
